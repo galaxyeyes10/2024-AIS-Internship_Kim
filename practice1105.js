6b411c51dcd8d12f262a9a413ec9c64b5edff6be
Kim Minchan <galaxyeyes02@naver.com>
2024-11-06 16:06:34 +0900

    fix : js_add_onLoad

--- practice1105.js
+++ practice1105.js
@@ -1,3 +1,17 @@
+function handleOnLoad() {
+    
+    const container = document.querySelectorAll(".container img");
+    for (let index = 0; index < container.length; index++) {
+        const element = container[index];
+
+            element.addEventListener("mouseover", ()=> {
+                scaleAndOpacity(element, 1.3, 1);});
+    
+            element.addEventListener("mouseout", ()=> {
+                scaleAndOpacity(element, 1, 0.5);});        
+    }
+}
+
 function scaleAndOpacity(image, scale, opacity) {
     image.style.transform = `scale(${scale})`;
     image.style.opacity = opacity;
